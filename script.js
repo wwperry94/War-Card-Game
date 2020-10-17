a0dedb59fbf8fc32dd54ed023b8b48cde27e2a49
wwperry94 <wwperry94@gmail.com>
2020-10-17 17:02:18 -0500

    redefined class

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let playCards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
 let trashCards = [];
 let BeginGame = document.getElementById("begin-btn");
-
+let p1Deck = document.getElementById('player-one-deck');
+let p2Deck = document.getElementById('player-two-deck');
 // Game Setup \\
 function beginGame() {
     let playerOne = document.getElementById("player-one-input").value;
@@ -24,77 +25,57 @@ function shuffle(array) {
 };
 BeginGame.addEventListener('click', () => {
     beginGame();
-    p1Deck = document.getElementById('player-one-deck');
-    p2Deck = document.getElementById('player-two-deck');
 });
 // function 
 
 // Core \\
 class Player {
-    constructor(paName) {
-        // this.playCards = playCards;
-        // this.value = value;
-        // let count = this.playCards.length,
-        let playArea = document.getElementById(paName);
-        //     fragment1 = document.createDocumentFragment();
-        // for (let i = 0; i < count; i++) {
-        //     let card = document.createElement('div');
-        //     card.id = `${[this.playCards.length]}`;
-        //     card.style.position = 'absolute';
-        //     card.style.backgroundColor = "black";
-        //     card.style.height = `150px`;
-        //     card.style.width = `100px`;
-        //     card.style.color = `red`;
-        //     card.style.display = 'flex';
-        //     card.style.justifyContent = 'center';
-        //     card.style.alignItems = `center`;
-        //     card.style.fontSize = `40px`;
-        //     card.textContent = `P1`;
-        //     fragment1.appendChild(card);
-        // }
-        // playArea.appendChild(fragment1);
+    constructor(paName, pArea) {
+        this.pArea = pArea;
+        // this.deckArr = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+        this.deckArr = [2, 3, 10, 12, 14];
+        this.trashArr = [];
     };
     random_item = () => {
-        //this returns the card value
-        console.log("play cards", playCards);
-        let value = playCards[Math.floor(Math.random() * playCards.length)];
-        console.log("value", value);
+        let value = this.deckArr[Math.floor(Math.random() * this.deckArr.length)];
+        return value;
     };
     playCard = () => {
-        // console.log("card val", this.value);
-        this.random_item();
-        // console.log("b4 splice, remaining cards", this.playCards)
-        // let idx = this.playCards.indexOf(this.randomCard);
-        // console.log("index of value", idx)
-        // this.playCards.splice(idx, 1);
-        // trashCards.push(this.randomCard);
-        // console.log("p1 trash pile", trashCards);
-        // console.log("after splice, remaining cards", this.playCards)
-        // if (this.playCards.length == 0) {
-        //     p1Deck.innerHTML = "Shuffle"
-        // } else {
-        //     p1Deck.innerHTML = this.randomCard;
-        // };
+        let value = this.getValue()[0];
+        if (value) {
+            console.log("this.value", value);
+            let idx = this.deckArr.indexOf(value);
+            this.deckArr.splice(idx, 1);
+            this.trashArr.push(value);
+            // console.log("trash", this.trashArr);
+            // console.log("after splice, remaining cards", this.deckArr);
+            if (this.deckArr.length == 0) {
+                this.pArea.innerHTML = "Shuffle!"
+            } else {
+                this.pArea.innerHTML = value;
+            };
+        }; 
     };
-    // getValue = () => {
-    //     return this.randomCard;
-    // };
+    getValue = () => {
+        let arr = [this.random_item(), this.trashArr];
+        // console.log("value", arr[0], "trash array", arr[1]);
+        return arr;
+    };
 };
-let p1 = new Player('player-one-deck');
-let p2 = new Player('player-two-deck');
+let p1 = new Player('player-one-deck', p1Deck);
+let p2 = new Player('player-two-deck', p2Deck);
 
 let flip = document.getElementById('play-btn');
 flip.addEventListener('click', () => {
     p1.playCard();
     p2.playCard();
-    // compareCards(p1.getValue(), p2.getValue());
+    compareCards(p1.getValue()[0], p2.getValue()[0]);
 });
 
 function compareCards(val1, val2) {
     if (val1 > val2) {
         console.log("p1 wins");
-        trashCards.push(val1, val2);
-        console.log("val1", val1, "val2", val2);
+        // console.log("val1", val1, "val2", val2);
     }
     else if (val2 > val1) {
         console.log("p2 wins");
